28bb80d793b5c5d516245d463197a5c5605460c2
Jacob Mischka <jacob@mischka.me>
2022-03-22 05:24:08 -0500

    Update pyproject.toml scripts and meta

--- src/demos/__init__.py
+++ src/demos/__init__.py
@@ -1,5 +1,21 @@
 import importlib
+from argparse import ArgumentParser
 
 
 def run_demo(demo: str):
     importlib.import_module("." + demo, package="demos")
+
+
+def main():
+    parser = ArgumentParser("run_demo")
+    parser.add_argument(
+        "demo_name",
+        help="Filename of the demo without the file extension, relative to demos directory",
+    )
+    args = parser.parse_args()
+
+    run_demo(args.demo_name)
+
+
+if __name__ == "__main__":
+    main()
