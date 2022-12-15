4b11dfeba62e1a0fac588a078b7ffd8e6d529f1d
Jacob Mischka <jacob@mischka.me>
2022-12-15 15:41:35 -0600

    Split playwright, context, and page into own fixtures

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,10 +1,16 @@
 # pylint: disable=redefined-outer-name
 
-import asyncio, os.path
+import asyncio
 from typing import AsyncIterator
 
 import pytest
-from playwright.async_api import BrowserType, Page, async_playwright
+from playwright.async_api import (
+    BrowserContext,
+    BrowserType,
+    Page,
+    Playwright,
+    async_playwright,
+)
 
 from interval_sdk import Interval
 
@@ -17,28 +23,41 @@ def config() -> Config:
 
 
 @pytest.fixture(scope="session")
-async def page(
-    config: Config,
+async def playwright() -> AsyncIterator[Playwright]:
+    async with async_playwright() as playwright:
+        yield playwright
+
+
+@pytest.fixture(scope="session")
+async def context(
+    playwright: Playwright,
     browser_name: str,
     browser_type_launch_args: dict,
+) -> AsyncIterator[BrowserContext]:
+    # tests_dir = os.path.dirname(os.path.realpath(__file__))
+    browser_context_args: dict = {}
+
+    browser_type: BrowserType = getattr(playwright, browser_name)
+    browser = await browser_type.launch(
+        # tests_dir + "/.session.json",
+        **{
+            **browser_type_launch_args,
+            **browser_context_args,
+        }
+    )
+    context = await browser.new_context()
+    yield context
+    await context.close()
+
+
+@pytest.fixture(scope="session")
+async def page(
+    config: Config,
+    context: BrowserContext,
 ) -> AsyncIterator[Page]:
-    async with async_playwright() as playwright:
-        # tests_dir = os.path.dirname(os.path.realpath(__file__))
-        browser_context_args: dict = {}
-
-        browser_type: BrowserType = getattr(playwright, browser_name)
-        browser = await browser_type.launch(
-            # tests_dir + "/.session.json",
-            **{
-                **browser_type_launch_args,
-                **browser_context_args,
-            }
-        )
-        context = await browser.new_context()
-
-        page = await config.log_in(context)
-        yield page
-        await context.close()
+    page = await config.log_in(context)
+    yield page
+    await context.close()
 
 
 @pytest.fixture
