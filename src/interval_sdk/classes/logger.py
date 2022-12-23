fe35e920dd5bff46661ea43f4e7e62f32b82a7a4
Jacob Mischka <jacob@mischka.me>
2022-12-23 12:38:11 -0600

    Add new methods to Logger

--- src/interval_sdk/classes/logger.py
+++ src/interval_sdk/classes/logger.py
@@ -1,28 +1,83 @@
 import sys, traceback
 from typing import TypeAlias, Literal
 
-LogLevel: TypeAlias = Literal["prod", "debug"]
+from interval_sdk.internal_rpc_schema import SdkAlert
+
+LogLevel: TypeAlias = Literal["quiet", "info", "debug"]
+
+CHANGELOG_URL = "https://interval.com/changelog"
+
+WARN_EMOJI = "\u26A0\uFE0F"
+ERROR_EMOJI = "‼️"
 
 
 class Logger:
-    log_level: LogLevel = "prod"
+    log_level: LogLevel = "info"
 
-    def __init__(self, log_level: LogLevel = "prod"):
-        self.log_level = log_level
+    def __init__(self, log_level: LogLevel | None = None):
+        if log_level is not None:
+            self.log_level = log_level
 
     def prod(self, *args, **kwargs):
-        print("[Interval]", *args, **kwargs)
+        """Important messages, always emitted"""
+        print("[Interval] ", *args, **kwargs)
 
-    def warn(self, *args, **kwargs):
-        print(*args, **kwargs, file=sys.stderr)
+    def prod_no_prefix(self, *args, **kwargs):
+        """Same as prod, but without the [Interval] prefix"""
+        print(*args, **kwargs)
 
     def error(self, *args, **kwargs):
-        print(*args, **kwargs, file=sys.stderr)
+        """Fatal errors or errors in user code, always emitted"""
+        print("[Interval] ", *args, **kwargs, file=sys.stderr)
+
+    def info(self, *args, **kwargs):
+        """Informational messages, not emitted in "quiet" log level"""
+        if self.log_level != "quiet":
+            print("[Interval] ", *args, **kwargs)
+
+    def info_no_prefix(self, *args, **kwargs):
+        """Same as info, but without the [Interval] prefix"""
+        if self.log_level != "quiet":
+            print(*args, **kwargs)
+
+    def warn(self, *args, **kwargs):
+        """Non-fatal warnings, not emitted in "quiet" log level"""
+        if self.log_level != "quiet":
+            print("[Interval] ", *args, **kwargs, file=sys.stderr)
 
     def debug(self, *args, **kwargs):
+        """Debugging/tracing information, only emitted in "debug" log level"""
         if self.log_level == "debug":
-            print(*args, **kwargs)
+            print("[Interval] ", *args, **kwargs)
 
     def print_exception(self, err: Exception):
         if self.log_level == "debug":
             traceback.print_exception(err, file=sys.stderr)
+
+    def handle_sdk_alert(self, sdk_alert: SdkAlert):
+        self.info_no_prefix()
+
+        if sdk_alert.severity == "INFO":
+            self.info("🆕\tA new Interval SDK version is available.")
+            if sdk_alert.message:
+                self.info(sdk_alert.message)
+        elif sdk_alert.severity == "WARNING":
+            self.warn(
+                f"{WARN_EMOJI}\tThis version of the Interval SDK has been deprecated. Please update as soon as possible, it will not work in a future update."
+            )
+            if sdk_alert.message:
+                self.warn(sdk_alert.message)
+            pass
+        elif sdk_alert.severity == "ERROR":
+            self.error(
+                f"{ERROR_EMOJI}\tThis version of the Interval SDK is no longer supported. Your app will not work until you update."
+            )
+            if sdk_alert.message:
+                self.error(sdk_alert.message)
+            pass
+        elif sdk_alert.message:
+            self.prod(sdk_alert.message)
+
+        self.info("\t- See what's new at:", CHANGELOG_URL)
+
+        self.info_no_prefix()
