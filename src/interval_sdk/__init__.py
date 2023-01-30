6ca81d545730dc88c895ce88b1111cf19e6e967e
Jacob Mischka <jacob@mischka.me>
2023-01-30 12:58:22 -0600

    Add Table and GridDataFetcherState types to package __init__.py

--- src/interval_sdk/__init__.py
+++ src/interval_sdk/__init__.py
@@ -4,3 +4,5 @@ from .io_schema import FileState, FileUrlSet
 from .classes.io import IO
 from .classes.io_error import IOError
 from .classes.layout import Layout
+from .components.table import TableDataFetcherState, FetchedTableData
+from .components.grid import GridDataFetcherState, FetchedGridData
